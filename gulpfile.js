412b28853e16155fa7ac89ef087beb490f034520
Chris Mittell <mittell@hotmail.co.uk>
2022-10-26 15:33:01 +0900

    feat: added import example

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-	return src('src/scss/index.scss').pipe(sass()).pipe(dest('src/css'));
+	return src('*.scss').pipe(sass()).pipe(dest('src/css'));
 }
 
 function watchTask() {
-	watch(['src/scss/index.scss'], buildStyles);
+	watch(['*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
