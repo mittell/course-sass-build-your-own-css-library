a14f1a08acf35c13963b03593bff34ec3a04876a
Chris Mittell <mittell@hotmail.co.uk>
2022-10-27 14:14:26 +0900

    feat: added purgecss example

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-	return src('src/scss/**/*.scss').pipe(sass()).pipe(dest('./src/css'));
+	return src('src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(
+			purgecss({
+				content: ['src/**/*.html'],
+			})
+		)
+		.pipe(dest('./src/css'));
 }
 
 function watchTask() {
-	watch(['src/scss/**/*.scss'], buildStyles);
+	watch(['src/scss/**/*.scss', 'src/**/*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
